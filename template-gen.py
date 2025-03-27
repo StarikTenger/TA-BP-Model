844967f5bbc6837240a0f3f3449ff8f83744963c
Andrei Ilin <stariktenger@gmail.com>
2025-03-27 13:36:03 +0100

    advanced template gen

--- template-gen.py
+++ template-gen.py
@@ -1,7 +1,19 @@
 import re
 import sys
+import argparse
 
-filename = sys.argv[1]
+parser = argparse.ArgumentParser(description="Generate TLA configuration from input file")
+parser.add_argument("filename", help="Input file name")
+parser.add_argument("--base", default=False, action="store_true", help="Enable base template generation")
+parser.add_argument("--name", default="Test", help="Specify benchmark name")
+parser.add_argument("--time", type=int, default=0, help="Specify time bound")
+
+args = parser.parse_args()
+
+filename = args.filename
+gen_base_template = args.base
+bench_name = args.name
+time_bound = args.time
 
 instructions = []
 
@@ -14,8 +26,13 @@ with open(filename, "r") as file:
             label = match.group(3) if match.group(3) else None
             deps = match.group(4).split(",") if match.group(4) else []
             lats = []
+
             if match.group(5):
                 lats = list(map(int, match.group(5).split("|")))
+
+                if gen_base_template:
+                    lats = lats[0:1]
+
                 instructions.append({
                 "depth": depth,
                 "type": type,
@@ -24,16 +41,20 @@ with open(filename, "r") as file:
                 "lats": lats
                 })
 
-branch_stack = []
 
+print(f"---- MODULE {bench_name} ----")
+print("EXTENDS TLC, Pipeline")
+print("const_prog == ⟨")
+
+branch_stack = []
 idx = 0
 for instruction in instructions:
     idx += 1
     while branch_stack and branch_stack[-1][1] >= instruction["depth"]:
         branch_stack.pop()
+    spec_of = ", ".join(str(idx + 1) for idx, _ in branch_stack)
     if instruction["type"].startswith("BR"):
         branch_stack.append((instructions.index(instruction), instruction["depth"]))
-    spec_of = ", ".join(str(idx + 1) for idx, _ in branch_stack)
     lat_fu = ", ".join(map(str, instruction['lats']))
     data_deps = {idx for dep in instruction["deps"] if dep for idx, instr in enumerate(instructions, start=1) if instr["label"] == dep}
     data_deps = data_deps if data_deps else {}
@@ -48,3 +69,16 @@ for instruction in instructions:
         .expandtabs(8)
     )
 
+print("⟩")
+print("const_superscalar == 1")
+if (gen_base_template):
+    print("const_BranchDivergence == FALSE")
+else:
+    print("const_BranchDivergence == TRUE")
+    
+if time_bound > 0:
+    print(f"TimeBounded == ¬TimeExceed({time_bound})")
+else:
+    print("TimeBounded == TRUE")
+
+print("====")
