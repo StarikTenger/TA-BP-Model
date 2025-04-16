acfc85fc79f8601ff7dad8b63a5314bbedc66317
Andrei Ilin <stariktenger@gmail.com>
2025-04-16 17:06:29 +0200

    ignore some files

--- run-cpp.sh
+++ run-cpp.sh
@@ -2,6 +2,6 @@ cd cpp_version
 g++ -std=c++20 -o main main.cpp
 ./main $1
 cd ..
-python3 diagram-diag.py cpp_version/out2.tmp >out.trd
-echo "------------------------------------" >>out.trd
-python3 diagram-diag.py cpp_version/out1.tmp >>out.trd
+python3 diagram-diag.py cpp_version/out2.tmp >tmp/out.trd
+echo "------------------------------------" >>tmp/out.trd
+python3 diagram-diag.py cpp_version/out1.tmp >>tmp/out.trd
