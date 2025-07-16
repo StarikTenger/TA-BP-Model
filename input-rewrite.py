69086d2075cca7c58a656ba4db801ad9805be71e
Andrei Ilin <stariktenger@gmail.com>
2025-07-16 14:07:52 +0200

    better input rewrite

--- input-rewrite.py
+++ input-rewrite.py
@@ -14,24 +14,27 @@ branch_variations = set()
 line_count = 0
 with open(filename, "r") as file:
     for line in file:
-        match = re.match(r"^([ \t]*)(\w+)(?:\s+#(\w+))?(?:\s+@([\w,]+))?.*\[([\d|]+)\]\s*(\*?)", line)
+        match = re.match(r"^([ \t]*)(\w+)(?:\s+#(\w+))?(?:\s+(@[\w]+(?:\s+@[\w]+)*))?.*\[([\d|]+)\]\s*(\*?)", line)
         if match:
             indent = match.group(1)
             depth = indent.count('\t') + indent.count(' ') // 4
             type = match.group(2)
             label = match.group(3) if match.group(3) else None
-            deps = match.group(4).split(",") if match.group(4) else []
+            deps = [dep.lstrip('@') for dep in match.group(4).split()] if match.group(4) else []
             lats = []
+            br_var = match.group(6) == '*'
 
             if match.group(5):
                 lats = list(map(int, match.group(5).split("|")))
+                lats.reverse()
 
                 instructions.append({
                 "depth": depth,
                 "type": type,
                 "label": label,
                 "deps": deps,
-                "lats": lats
+                "lats": lats,
+                "br_var": br_var
                 })
 
             if match.group(6):
@@ -59,11 +62,11 @@ def idx_to_alpha(i):
     return letters
 
 for idx, instruction in enumerate(instructions, start=1):
-    label = "\\textit{" + idx_to_alpha(idx) + ":}"
+    label = "\\textit{" + ("*" if instruction["br_var"] else "") + idx_to_alpha(idx) + ":}"
     data_deps = {j for dep in instruction["deps"] if dep for j, instr in enumerate(instructions, start=1) if instr["label"] == dep}
     deps_alpha = [idx_to_alpha(j) for j in sorted(data_deps)] if data_deps else []
     deps_str = "" if len(deps_alpha) == 0 else "$\{" + ", ".join(deps_alpha) + "\}$"
-    lats_str = "$[" + "|".join(map(str, instruction['lats'])) + "]$"
+    lats_str = "$" + "|".join(map(str, instruction['lats'])) + "$"
     # Prepare row: label, skip depth columns, then type, then fill, then deps, lats
     row = [] + [""] * instruction["depth"]
     row += [label]
