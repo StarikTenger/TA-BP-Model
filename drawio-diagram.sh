bea822df3a20366b9174b2f6b09ee5cc3e199ec6
Andrei Ilin <stariktenger@gmail.com>
2025-07-29 14:15:57 +0200

    event resolution scripts: 1st frame probleb fix

--- drawio-diagram.sh
+++ drawio-diagram.sh
@@ -1,7 +1,8 @@
 input=$1
 
 cat $input >tmp/event_table.tmp
+echo >>tmp/event_table.tmp
+echo >>tmp/event_table.tmp
 ./cpp_version/build/event_table $input >>tmp/event_table.tmp
-
 python3 drawio-diagram.py --graph tmp/event_table.tmp --outfile tmp/test.drawio tmp/input.instr_compare.trd
 drawio -x tmp/test.drawio -s 2 -f png
--- event-resolution.sh
+++ event-resolution.sh
@@ -1,6 +1,8 @@
 input=$1
 
 cat $input >event_table.tmp
+echo >>event_table.tmp
+echo >>event_table.tmp
 ./cpp_version/build/event_table $input >>event_table.tmp
 python3 EventDiagram.py <event_table.tmp
 # rm event_table.tmp
